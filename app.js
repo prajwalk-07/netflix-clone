89c8e2a030715791e079c10a24b066dbf3e059e3
prajwalk2003 <prajwalkr2003@gmail.com>
2023-11-15 12:06:37 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,70 @@
+var closeSpan3=document.querySelector("#close3")
+var addSpan3=document.querySelector("#add3")
+document.querySelector("#btn3").addEventListener("click", function() {
+        var p3=document.querySelector("#p3")
+        p3.style.display=(p3.style.display==="none")?"block":"none"
+        closeSpan3.style.display=(closeSpan3.style.display==="none")?"block":"none"
+        addSpan3.style.display=(addSpan3.style.display==="block")?"none":"block"
+    
+})
+var closeSpan4=document.querySelector("#close4")
+var addSpan4=document.querySelector("#add4")
+document.querySelector("#btn4").addEventListener("click", function() {
+    var p4=document.querySelector("#p4")
+    p4.style.display=(p4.style.display==="none")?"block":"none"
+    closeSpan4.style.display=(closeSpan4.style.display==="none")?"block":"none"
+    addSpan4.style.display=(addSpan4.style.display==="block")?"none":"block"
+
+})
+var closeSpan5=document.querySelector("#close5")
+var addSpan5=document.querySelector("#add5")
+document.querySelector("#btn5").addEventListener("click", function() {
+    var p5=document.querySelector("#p5")
+    p5.style.display=(p5.style.display==="none")?"block":"none"
+    closeSpan5.style.display=(closeSpan5.style.display==="none")?"block":"none"
+    addSpan5.style.display=(addSpan5.style.display==="block")?"none":"block"
+
+})
+var closeSpan6=document.querySelector("#close6")
+var addSpan6=document.querySelector("#add6")
+document.querySelector("#btn6").addEventListener("click", function() {
+    var p6=document.querySelector("#p6")
+    p6.style.display=(p6.style.display==="none")?"block":"none"
+    closeSpan6.style.display=(closeSpan6.style.display==="none")?"block":"none"
+    addSpan6.style.display=(addSpan6.style.display==="block")?"none":"block"
+
+})
+var closeSpan7=document.querySelector("#close7")
+var addSpan7=document.querySelector("#add7")
+document.querySelector("#btn7").addEventListener("click", function() {
+    var p7=document.querySelector("#p7")
+    p7.style.display=(p7.style.display==="none")?"block":"none"
+    closeSpan7.style.display=(closeSpan7.style.display==="none")?"block":"none"
+    addSpan7.style.display=(addSpan7.style.display==="block")?"none":"block"
+
+})
+var closeSpan8=document.querySelector("#close8")
+var addSpan8=document.querySelector("#add8")
+document.querySelector("#btn8").addEventListener("click", function() {
+    var p8=document.querySelector("#p8")
+    p8.style.display=(p8.style.display==="none")?"block":"none"
+    closeSpan8.style.display=(closeSpan8.style.display==="none")?"block":"none"
+    addSpan8.style.display=(addSpan8.style.display==="block")?"none":"block"
+
+})
+// var p1=document.querySelector("#p1")
+
+// let clicked=true
+// function toggle(){
+//     if(clicked){
+//         p1.style.display="block"
+//         clicked=false
+//         closeSpan.style.display="block"
+//         addSpan.style.display="none"
+//     }else{
+//         p1.style.display="none"
+//         clicked=true
+//         closeSpan.style.display="none"
+//         addSpan.style.display="block"
+//     }
+// }
